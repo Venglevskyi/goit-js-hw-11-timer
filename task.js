e1de68ed2ec0d2c948706d5659f0a88c1a4e0d5e
Roman <Roman.Vengl1@gmail.com>
2019-11-26 17:44:11 +0200

    added task

--- task.js
+++ task.js
@@ -1,9 +1,50 @@
 class CountdownTimer {
-    
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  timer() {
+    this.timerId = setInterval(() => {
+      const futureTime = Date.parse(this.targetDate);
+      const deltaTime = futureTime - Date.now();
+      updateTimer(deltaTime);
+      if (deltaTime <= 0) {
+        clearInterval(this.timerId);
+      }
+    }, 1000);
+  }
 }
 
+function updateTimer(delta) {
+  const time = delta;
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-const CountdownTimer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
-  });
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+const Timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Dec 31, 2019")
+});
+
+const refs = {
+  days: document.querySelector('.value[data-value="days"]'),
+  hours: document.querySelector('.value[data-value="hours"]'),
+  mins: document.querySelector('.value[data-value="mins"]'),
+  secs: document.querySelector('.value[data-value="secs"]')
+};
+
+Timer.timer();
